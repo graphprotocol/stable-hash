c4313dc68fa98ca0050bccf7dc0e67ea94bcb8e4
Theo Butler <theodusbutler@gmail.com>
2021-12-22 14:03:32 -0300

    WIP: parallel buffer explicit SIMD

--- src/fld_mixer_simd.rs
+++ src/fld_mixer_simd.rs
@@ -1,7 +1,8 @@
 use super::fld_mixer::*;
+use packed_simd::u128x4;
 use std::num::Wrapping;
 
-const BUFFER: usize = 8;
+const BUFFER: usize = 4;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct FldMixSimd {
@@ -23,8 +24,21 @@ impl FldMix for FldMixSimd {
         self.buffer[self.index].1 = Wrapping(other as u128);
         self.index += 1;
         if self.index == BUFFER {
-            for row in &mut self.buffer {
-                row.0 = Self::uv(row.0, row.1);
+            let p = u128x4::splat(P.0);
+            let q = u128x4::splat(Q.0);
+            let r = u128x4::splat(R.0);
+            let y = u128x4::new(
+                self.buffer[0].1 .0,
+                self.buffer[1].1 .0,
+                self.buffer[2].1 .0,
+                self.buffer[3].1 .0,
+            );
+            let a = r * y + q;
+            let b = q * y + p;
+            for i in 0..BUFFER {
+                let a_i = Wrapping(unsafe { a.extract_unchecked(i) });
+                let b_i = Wrapping(unsafe { b.extract_unchecked(i) });
+                self.buffer[i].0 = self.buffer[i].0 * a_i + b_i;
             }
             self.index = 0;
         }
@@ -32,8 +46,21 @@ impl FldMix for FldMixSimd {
 
     fn finalize(&self) -> u128 {
         let mut x = IDENTITY;
-        for row in &self.buffer {
-            x = Self::uv(x, row.0);
+        let p = u128x4::splat(P.0);
+        let q = u128x4::splat(Q.0);
+        let r = u128x4::splat(R.0);
+        let y = u128x4::new(
+            self.buffer[0].0 .0,
+            self.buffer[1].0 .0,
+            self.buffer[2].0 .0,
+            self.buffer[3].0 .0,
+        );
+        let a = r * y + q;
+        let b = q * y + p;
+        for i in 0..BUFFER {
+            let a_i = Wrapping(unsafe { a.extract_unchecked(i) });
+            let b_i = Wrapping(unsafe { b.extract_unchecked(i) });
+            x = x * a_i + b_i;
         }
         for i in 0..self.index {
             x = Self::uv(x, self.buffer[i].1);
