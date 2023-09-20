e50aabef55b8c4de581ca5c4ffa7ed8beed7e998
Filipe Azevedo <filipe@azevedo.io>
2023-09-20 10:44:14 +0100

    As unordered set (#17)
    * Add AsUnorderedSet
    
    * v0.4.4
    
    * Looser restrictions for AsUnorderedSet types
    
    * update dependencies
    
    ---------
    
    Co-authored-by: Zac Burns <That3Percent@gmail.com>

--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -10,24 +10,6 @@ mod vec;
 
 use crate::prelude::*;
 
-pub(self) fn unordered_unique_stable_hash<H: StableHasher>(
-    items: impl Iterator<Item = impl StableHash>,
-    field_address: H::Addr,
-    state: &mut H,
-) {
-    profile_fn!(unordered_unique_stable_hash);
-
-    for member in items {
-        // Must create an independent hasher to "break" relationship between
-        // independent field addresses.
-        // See also a817fb02-7c77-41d6-98e4-dee123884287
-        let mut new_hasher = H::new();
-        let (a, b) = field_address.unordered();
-        member.stable_hash(a, &mut new_hasher);
-        state.write(b, new_hasher.to_bytes().as_ref());
-    }
-}
-
 impl<'a, T: StableHash> StableHash for &'a T {
     #[inline]
     fn stable_hash<H: StableHasher>(&self, field_address: H::Addr, state: &mut H) {
